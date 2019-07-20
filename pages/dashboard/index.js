19245d54539406818eff7799a7076481fe4c1973
Bumbleboss <bumbleboss46@gmail.com>
2019-07-20 17:22:45 +0300

    posters automation now works - Changed how the request system works. Should be better than before although it lacks performance.. caching required - Changed the styling for posters as its meant to be modular so the child elements are now applied to the poster-dashboard class - Added animations to be used later for show title

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -2,6 +2,8 @@ if(!remote.getGlobal('darwin')) {
   document.getElementById('dragger').remove()
 }
 
+createPosters()
+
 function signout() {
   remote.getGlobal('disconnect')()
 }
@@ -10,105 +12,127 @@ function show(x) {
   let par = x.parentElement.parentElement;
   [...par.children].forEach(element => {
     if(element.children[0] == x) {
-      x.classList.add("selected");
+      x.classList.add('selected');
     } else {
-      element.children[0].classList.remove("selected");
+      element.children[0].classList.remove('selected');
     }
   });
 }
 
-function testFanart() {
-   let img = document.createElement('img')
-   remote.getGlobal('fanart').shows.get(75682).then(res => {
-      img.src = res.seasonposter[0].url
-   })
-   document.body.appendChild(img)
+function syncWatched() {
+  return tr.sync.watched({
+    type: 'shows'
+  }).then(res => res)
 }
 
+function hiddenItems() {
+  return tr.users.hidden.get({
+    section: 'progress_watched',
+    limit: 100
+  }).then(res => res)
+}
 
-function testPoster() {
-  let tr = remote.getGlobal('trakt')
-  tr.sync.watched({
-    type: 'shows'
-  }).then(res => {
-    let shows = []
-    new Promise(async (resolve, reject) => {
-      for(let i = 0; i < res.length; i++) {
-        let slug = res[i].show.title
-        let id = res[i].show.ids.trakt
-        tr.shows.progress.watched({
-          id: id,
-          extended: 'full',
-        }).then(res2 => {
-          if(res2.aired > res2.completed) {
-            let ep = res2.next_episode
-            shows.push({
-              id: id,
-              info: {
-                title: slug,
-                subtitle: `${ep.season} x ${ep.number} (${ep.number_abs}) ${ep.title}`,
-                rating: ep.rating,
-                id: res[i].show.ids.tvdb
-              }
-            })        
-          }
-        }).catch(err => console.log(err))
-      }
-      resolve(shows) 
-    }).then(show => {
-      console.log(show[0])
+function resolveAll() {
+  return Promise.all([syncWatched(), hiddenItems()])
+}
+
+function createPosters() {
+  resolveAll().then(([res, res2]) => {
+    let arr = Array.from(res);
+    let arr2 = Array.from(res2);
+
+    // filters hidden items
+    let array2Ids = arr2.map(item => item.show.ids.trakt);
+    arr = arr.filter((item) => !array2Ids.includes(item.show.ids.trakt));
+
+    // filters completed shows
+    arr.forEach(item => {
+      tr.shows.progress.watched({
+        id: item.show.ids.trakt,
+        extended: 'full'
+      }).then(res4 => {
+        if(res4.aired > res4.completed) {
+          let ep = res4.next_episode
+          createPoster({
+            title: item.show.title,
+            subtitle: `${ep.season} x ${ep.number} (${ep.number_abs}) ${ep.title}`,
+            rating: ep.rating,
+            id: item.show.ids.tvdb
+          })
+        }
+      }).catch(err => console.log(err))
     })
-  })
+  }).catch(err => console.log(err))
 }
 
 function createPoster(x) {
-  let li = document.createElement("li")
-  li.classList = "poster poster-dashboard shadow_h"
+  let li = document.createElement('li')
+  li.classList = 'poster poster-dashboard shadow_h'
+  li.setAttribute('title', x.title)
+  li.setAttribute('subtitle', x.subtitle)
 
-  let poster_content = document.createElement("div")
-  poster_content.classList = "poster-content"
+  let poster_content = document.createElement('div')
+  poster_content.classList = 'poster-content'
 
-  let poster_content_left = document.createElement("div")
-  poster_content_left.classList = "poster-content-left fs14 white_t fw700"
+  let poster_content_left = document.createElement('div')
+  poster_content_left.classList = 'poster-content-left fs14 white_t fw700'
 
-  let heart = document.createElement("img")
-  heart.src = "../../assets/icons/app/heart.svg"
+  let heart = document.createElement('img')
+  heart.src = '../../assets/icons/app/heart.svg'
 
-  let rate = document.createElement("span")
+  let rate = document.createElement('span')
   rate.innerText = `${Math.round(x.rating*10)}%`
 
   poster_content_left.appendChild(heart)
   poster_content_left.appendChild(rate)
 
-  let poster_content_right = document.createElement("div")
-  poster_content_right.classList = "poster-content-right fs14 white_t fw700 t_"
-  poster_content_right.innerText = "Add to History"
+  let poster_content_right = document.createElement('div')
+  poster_content_right.classList = 'poster-content-right fs14 white_t fw700 t_'
+  poster_content_right.innerText = 'Add to History'
 
   poster_content.appendChild(poster_content_left)
   poster_content.appendChild(poster_content_right)
 
-  let img = document.createElement("img")
+  let img = document.createElement('img')
 
-  remote.getGlobal('fanart').shows.get(x.id).then(res => {
+  fr.shows.get(x.id).then(res => {
     if(res) {
       if(res.seasonposter) {
         img.src = res.seasonposter[0].url
       }else if(res.tvposter) {
         img.src = res.tvposter[0].url
       }else{
-        img.src = "https://png.pngtree.com/svg/20160504/39ce50858b.svg"
+        img.src = 'https://png.pngtree.com/svg/20160504/39ce50858b.svg'
       }
     }
-  }).catch(img.src = "https://png.pngtree.com/svg/20160504/39ce50858b.svg")
-
-  let text = document.createElement("div")
-  text.style = "display:none"
-  text.innerHTML = `${x.title}<span>${x.subtitle}</span>`
+  }).catch(img.src = 'https://png.pngtree.com/svg/20160504/39ce50858b.svg')
 
   li.appendChild(poster_content)
   li.appendChild(img)
-  li.appendChild(text)
 
-  let posters = document.getElementById("posters");
+  let posters = document.getElementById('posters')
   posters.appendChild(li);
+}
+
+function createTitle() {
+  let title = document.getElementById('poster_title')
+  let info = document.getElementById('posters').firstChild
+
+  console.log(info.classList)
+
+  let h3 = document.createElement('h3')
+  h3.classList = 'h3 red_t tu'
+  h3.innerText = 'up next to watch'
+
+  let h1 = document.createElement('h1')
+  h1.classList = 'h1 white_t tu'
+  h1.innerText = info.getAttribute('title')
+
+  let h1_2 = document.createElement('h1')
+  h1_2.classList = 'h1 white_d_t'
+  h1_2.innerText = info.getAttribute('subtitle')
+
+  title.appendChild(h3)
+  title.appendChild(h1)
+  title.appendChild(h1_2)
 }
