6c5d8d879de5899d670f79b85421ce90f3e16f70
Bob Walter <bob.walter@outlook.de>
2019-08-03 01:59:39 +0200

    loading alert
    If the app takes unexpected long to load, a popup offers the user to relaunch the app or to report an issue. Alert popup can be visually improved.

--- pages/loading/index.js
+++ pages/loading/index.js
@@ -1,7 +1,28 @@
 const trakt = remote.getGlobal('trakt')
+const relaunchApp = remote.getGlobal('relaunchApp')
+
 
 window.onload = function() {
+   let loadingTime = Date.now()
    ipcRenderer.send('loading-screen', 'loaded')
+
+   function alertChecker() {
+      setTimeout(() => {
+         let timeTaken = Date.now()-loadingTime
+         debugLog('loading check', timeTaken)
+   
+         if(timeTaken > 12e3) {
+            debugLog('loading', 'too long')
+            showAlertBox()
+         } else {
+            alertChecker()
+         }
+      }, 1e3)
+   }
+
+   alertChecker()
+   
+
 
    ipcRenderer.on('loading-screen', async (event, data) => {
       if(data === 'start') {
@@ -15,8 +36,23 @@ window.onload = function() {
          
          debugLog('loading', 'done')
          setTimeout(() => {
+            // telling the app to move on, we need this to trigger the opening of the dashboard page
             ipcRenderer.send('loading-screen', 'done')
-         }, 10) // giving some small extra timeout
+         }, 33.3) // giving some small extra timeout
       }
    })
+}
+
+
+function showAlertBox(text) {
+   let alert_box = document.createElement('div')
+   alert_box.classList.add('alert_box', 'white_t')
+   alert_box.innerHTML = `
+      <p>
+         Man, this takes long!
+         <br>
+         Try <span style="text-decoration: underline" onclick="relaunchApp()">relaunching</span> or reach out to us <span style="text-decoration: underline" href="https://github.com/CodingBobby/traktify/issues">here</span> to get help.
+      </p>
+   `
+   document.body.appendChild(alert_box)
 }
