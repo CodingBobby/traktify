6c3a091f20749a0b90e4fad2569f302b35054094
Bob Walter <bob.walter@outlook.de>
2020-04-08 01:07:30 +0200

    refactoring helper
    Moved functions to helper module and added tests and documentation. Some minor code modifications were made.

--- src/__tests__/helper.js
+++ src/__tests__/helper.js
@@ -1,9 +1,9 @@
 const {
-   inRange, clone, shadeHexColor
+   inRange, clone, shadeHexColor, startsWithFilter
 } = require('./../modules/helper.js')
 
 
-test('inRange function', () => {
+test('inRange Function', () => {
    expect(inRange(3, [-1, 5])).toBe(true)
    expect(inRange(7.123, [0.002159, 7.1230001])).toBe(true)
    expect(inRange(Math.PI, [Math.E, 2*Math.E])).toBe(true)
@@ -12,7 +12,7 @@ test('inRange function', () => {
 })
 
 
-test('clone function', () => {
+test('clone Function', () => {
    let foo = {
       var: 1000
    }
@@ -27,4 +27,28 @@ test('shadeHexColor Function', () => {
    expect(shadeHexColor('#fff111', 30)).toBe('#ffff16')
    expect(shadeHexColor('#2b2b11', 30)).toBe('#373716')
    expect(shadeHexColor('#553355', 'string')).toBe('#ffffff')
+})
+
+
+test('startsWithFilter Function', () => {
+   let string = 's:Firefly'
+   let prefixes = ['s:', 'm:']
+   let result = startsWithFilter(string, prefixes, ':')
+
+   expect(result.hasOwnProperty('filtered')).toBe(true)
+   expect(result.filtered).toBe('Firefly')
+   expect(result.hasOwnProperty('found')).toBe(true)
+   expect(result.found).toBe('s')
+})
+
+
+test('startsWithFilter Function null', () => {
+   let string = 'p:Nathan Fillion'
+   let prefixes = ['s:', 'm:']
+   let result = startsWithFilter(string, prefixes, ':')
+
+   expect(result.hasOwnProperty('filtered')).toBe(true)
+   expect(result.filtered).toBe(string)
+   expect(result.hasOwnProperty('found')).toBe(true)
+   expect(result.found).toBe(null)
 })
