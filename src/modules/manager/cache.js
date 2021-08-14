bf7bb4813957bd1c3aedc3cb2da65aded5b8a2b0
Bob Walter <bob.walter@outlook.de>
2021-08-15 01:28:27 +0200

    cache handler
    Adding a retrieving system that makes different key statuses easier to handle.

--- src/modules/manager/cache.js
+++ src/modules/manager/cache.js
@@ -8,8 +8,8 @@ const PATHS = require('../app/paths.js')
 class Cache {
   /**
    * Wraps an instance of `flatCache` into a more specific setup.
-   * @param {String} name identifier of the Cache instance
-   * @param {Number} [cacheTime] time to keep data, 0 for never expire
+   * @param {string} name identifier of the Cache instance
+   * @param {number} [cacheTime] time to keep data, 0 for never expire
    */
   constructor(name, cacheTime=0) {
     this.name = name
@@ -18,12 +18,76 @@ class Cache {
     this.expire = cacheTime === 0 ? false : cacheTime * 1000 * 60
   }
 
+
+  /**
+   * Only checks the status of a key without modifying anything.
+   * @param {string} key identifier
+   * @returns {'empty'|'expired'|'valid'}
+   */
+  keyStatus(key) {
+    let now = new Date().getTime()
+    let value = this.cache.getKey(key)
+
+    if (value === undefined) {
+      return 'empty'
+    } else if (value.expire !== false && value.expire < now) {
+      return 'expired'
+    }
+  
+    return 'valid'
+  }
+
+
+  /**
+   * Should only be called when new data for the key is available.
+   * @callback UPDATE_CB
+   * @param {*} update new data
+   * @returns {void}
+   */
+
+  /**
+   * @callback EMPTY_CB
+   * @param {UPDATE_CB} onUpdates fires when the key should be (re)created
+   * @returns {void}
+   */
+
+  /**
+   * @callback VALID_CB
+   * @param {*} value the key's content
+   * @param {UPDATE_CB} onUpdates fires when updates should be written into the key
+   * @returns {void}
+   */
+
+  /**
+   * Handler for retrieval of the content of a key.
+   * Callbacks accept another callback to optionally send back updated data to be saved in the key.
+   * @param {string} key 
+   * @param {EMPTY_CB} onEmpty fires when key isn't present yet or was expired
+   * @param {VALID_CB} onValid fires when key contains data
+   */
+  retrieve(key, onEmpty, onValid) {
+    let value = this.getKey(key, true)
+
+    if (value === undefined) {
+      onEmpty(update => {
+        this.setKey(key, update)
+      })
+
+    } else {
+      onValid(value, update => {
+        this.setKey(key, update)
+      })
+    }
+  }
+
+
   /**
    * Retrieve data saved under given key.
-   * @param {String} key key which will be read
+   * @param {string} key key which will be read
+   * @param {boolean} [removeIfExpired] whether to remove key when expired
    * @returns {*} stored data
    */
-  getKey(key) {
+  getKey(key, removeIfExpired=false) {
     let now = new Date().getTime()
     let value = this.cache.getKey(key)
     if (value === undefined) {
@@ -31,8 +95,12 @@ class Cache {
 
     } else if (value.expire !== false && value.expire < now) {
       tracer.warn(`cache-key ${key} is expired`)
-      this.removeKey(key)
-      tracer.warn('removed expired key')
+
+      if (removeIfExpired) {
+        this.removeKey(key)
+        tracer.warn('removed expired key')
+      }
+      
       return undefined
       
     } else {
@@ -40,9 +108,10 @@ class Cache {
     }
   }
 
+
   /**
    * Save data under a given key.
-   * @param {String} key key that should represent the data
+   * @param {string} key key that should represent the data
    * @param {*} value data to be stored
    */
   setKey(key, value) {
@@ -53,13 +122,15 @@ class Cache {
     })
   }
 
+
   /**
    * Remove key from cache and delete associated data.
-   * @param {String} key key to remove
+   * @param {string} key key to remove
    */
   removeKey(key) {
     this.cache.removeKey(key)
   }
+
 
   /**
    * Write entire cache to disk.
@@ -71,6 +142,7 @@ class Cache {
     this.cache.save(true)
     tracer.log(`cache was saved in ${Date.now() - timer} ms`)
   }
+
 
   /**
    * Permanently delete entire cache.
