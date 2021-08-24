a923626c8f17498a8df6127b9a29e80b901e9713
Mohsen M <me@bumbleboss.xyz>
2021-08-24 23:40:25 +0200

    added functionality to the login page

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,19 +1,28 @@
-/*actions.children[0].addEventListener('click', () => {
+/**
+ * Code below handles the authentication of the user. 
+ * When login button is triggered it opens a verifcation url and button is replaced with two different buttons.
+ */
+ actions.children[0].addEventListener('click', () => {
   window.traktify.auth().then(result => {
     code.innerText = result.user_code;
     code.style.display = 'initial';
 
-    actions.innerHtml = `
-      <div class="fs18 fwMedium" value="${result.user_code}">copy code</div>
-      <div class="fs18 fwMedium" href="${result.verification_url}">open url</div>
-    `
+    // opens verification url on a browser tab
+    window.traktify.browse(result.verification_url);
 
+    // replaces login button with two different buttons
+    actions.innerHTML = `
+      <div class="fs18 fwMedium" data-code="${result.user_code}">copy code</div>
+      <div class="fs18 fwMedium" data-url="${result.verification_url}">open url</div>
+    `;
+
+    // adds functionality for the new buttons
     actions.children[0].addEventListener('click', (e) => {
-      navigator.clipboard.writeText(e.target.value)
+      navigator.clipboard.writeText(e.target.dataset.code)
     })
 
     actions.children[1].addEventListener('click', (e) => {
-      navigator.clipboard.writeText(e.target.value)
+      window.traktify.browse(e.target.dataset.url)
     })
   }).catch(err => console.error(err))
-}, {once: true});*/
+}, {once: true})
